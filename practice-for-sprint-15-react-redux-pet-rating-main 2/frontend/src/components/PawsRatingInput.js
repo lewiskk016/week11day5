6dcda84c576296d9f88f9d3986a7a10110fa4a59
lewiskk016 <lewiskk016@gmail.com>
2023-05-21 20:51:17 -0400

    sick but tried

--- practice-for-sprint-15-react-redux-pet-rating-main 2/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main 2/frontend/src/components/PawsRatingInput.js
@@ -1,34 +1,85 @@
+import { useState } from 'react';
+
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
+  const [activeRating, setActiveRating] = useState(rating);
 
+  const handleMouseEnter = (number) => {
+    setActiveRating(number);
+  };
+
+  const handleMouseLeave = () => {
+    setActiveRating(rating);
+  };
+
+  const handleClick = (number) => {
+    onChange(number);
+  };
+
+
+
+// import {useState} from 'react';
+
+// const PawsRatingInput = ({ rating, disabled, onChange }) => {
+//   const [activeRating, setActiveRating] = useState(rating);
+//   const handleMouseEnter = (number) => {
+//     setActiveRating(number);
+//   };
+//   const handleMouseLeave = () => {
+//     setActiveRating(rating);
+//   };
+//   const handleClick = (number) => {
+//     onChange(number);
+//   };
+  // return (
+
+
+  //   <>
+  //   <input
+  //     type="number"
+  //     disabled={disabled}
+  //     value={rating}
+  //     onChange={(event) => onChange(Number(event.target.value))}
+  //   />
+  //   <div className="rating-input">
+  //       <div className={rating >= 1 ? 'filled' : 'empty'}>
+  //         <i className="fa fa-paw"></i>
+  //       </div>
+  //       <div className={rating >= 2 ? 'filled' : 'empty'}>
+  //         <i className="fa fa-paw"></i>
+  //       </div>
+  //       <div className={rating >= 3 ? 'filled' : 'empty'}>
+  //         <i className="fa fa-paw"></i>
+  //       </div>
+  //       <div className={rating >= 4 ? 'filled' : 'empty'}>
+  //         <i className="fa fa-paw"></i>
+  //       </div>
+  //       <div className={rating >= 5 ? 'filled' : 'empty'}>
+  //         <i className="fa fa-paw"></i>
+  //     </div>
+  //   </div>
+  // </>
   return (
-
-
     <div>
-    <input
-      type="number"
-      disabled={disabled}
-      value={rating}
-      onChange={onChange}
-    />
-    <div className="rating-input">
-        <div className={rating >= 1 ? 'filled' : 'empty'}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div className={rating >= 2 ? 'filled' : 'empty'}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div className={rating >= 3 ? 'filled' : 'empty'}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div className={rating >= 4 ? 'filled' : 'empty'}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div className={rating >= 5 ? 'filled' : 'empty'}>
-          <i className="fa fa-paw"></i>
+      <input
+        type="number"
+        disabled={disabled}
+        value={rating}
+        onChange={(event) => onChange(Number(event.target.value))}
+      />
+      <div className="rating-input">
+        {[1, 2, 3, 4, 5].map((number) => (
+          <div
+            key={number}
+            className={activeRating >= number ? 'filled' : 'empty'}
+            onMouseEnter={() => handleMouseEnter(number)}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => handleClick(number)}
+          >
+            <i className="fa fa-paw"></i>
+          </div>
+        ))}
       </div>
     </div>
-  </div>
-
   );
 
 };
